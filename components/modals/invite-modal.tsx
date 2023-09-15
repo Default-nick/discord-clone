43848caee5b325986aa7b48159d5fb89933f4df7
Leandro Alves <lalvesevangelista@gmail.com>
2023-09-15 15:47:31 -0300

    feat: modal de sair do servidor e de excluir servidor

--- components/modals/invite-modal.tsx
+++ components/modals/invite-modal.tsx
@@ -58,7 +58,7 @@ export const InviteModal = () => {
       <DialogContent className="bg-white text-black p-0 overflow-hidden">
         <DialogHeader className="pt-8 px-6">
           <DialogTitle className="text-2xl text-center font-bold">
-            Convidar amigos
+            Convidar amigos para {server?.name}
           </DialogTitle>
         </DialogHeader>
         <div className="p-6">
