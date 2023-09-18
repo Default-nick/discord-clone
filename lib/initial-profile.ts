fd365b3df22a5ded2b5f69c87d2ad2ee60e1239c
Leandro Alves <lalvesevangelista@gmail.com>
2023-09-18 16:20:36 -0300

    refactor: Inclusao de username no banco de dados. feat: Listar canais ja criados, adicao de botao de criacao de canais.

--- lib/initial-profile.ts
+++ lib/initial-profile.ts
@@ -22,6 +22,7 @@ export const initialProfile = async () => {
     data: {
       userId: user.id,
       name: `${user.firstName} ${user.lastName}`,
+      username: `${user.username}`,
       imageUrl: user.imageUrl,
       email: user.emailAddresses[0].emailAddress,
     },
