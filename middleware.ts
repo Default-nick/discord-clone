1f15056c9890d403e75b9c74af71b92a41613854
Leandro Alves <lalvesevangelista@gmail.com>
2023-10-03 11:31:44 -0300

    refactor: correcoes no middleware

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,22 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
-  publicRoutes: ["/api/uploadthing"],
+  publicRoutes: ["/api/uploadthing", "/api/socket/io"],
+  ignoredRoutes: ["/((?!api|trpc))(_next|.+..+)(.*)", "/api/socket/io"],
+  afterAuth(auth, req, evt) {
+    // handle users who aren't authenticated
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+    // redirect them to organization selection page
+
+    return NextResponse.redirect("/");
+  },
 });
 
 export const config = {
